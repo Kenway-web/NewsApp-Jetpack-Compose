6fa6bcdef786d74696d8118228c48563036e9d32
Kenway-web <66524914+Kenway-web@users.noreply.github.com>
2023-12-12 01:07:29 +0530

    Error state handling and progress bar added

--- app/src/main/java/com/music/newsapp/presentation/news_screen/NewsScreen.kt
+++ app/src/main/java/com/music/newsapp/presentation/news_screen/NewsScreen.kt
@@ -2,6 +2,7 @@ package com.music.newsapp.presentation.news_screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,22 +12,31 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.music.newsapp.domain.model.Article
 import com.music.newsapp.presentation.component.CategoryTabRow
 import com.music.newsapp.presentation.component.NewsArticleCard
 import com.music.newsapp.presentation.component.NewsScreenTopBar
+import com.music.newsapp.presentation.component.RetryContent
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -37,62 +47,101 @@ fun NewsScreen(
     state: NewsScreenState,
     onEvent:(NewsScreenEvent) -> Unit
 ) {
-        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-        val pagerState = rememberPagerState()
-        val coroutineScope = rememberCoroutineScope()
-        val categories = listOf<String>(
-            "General","Business","Health","Science","Sports","Technology","Entertainment"
-        )
-    
-        LaunchedEffect(key1 = pagerState)
-        {
-           snapshotFlow { pagerState.currentPage }.collect{ page ->
-               onEvent(NewsScreenEvent.OnCategoryChanged(category = categories[page]))
-           }
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val pagerState = rememberPagerState()
+    val coroutineScope = rememberCoroutineScope()
+    val categories = listOf<String>(
+        "General", "Business", "Health", "Science", "Sports", "Technology", "Entertainment"
+    )
+
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    var shouldBottomSheetShow by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = pagerState)
+    {
+        snapshotFlow { pagerState.currentPage }.collect { page ->
+            onEvent(NewsScreenEvent.OnCategoryChanged(category = categories[page]))
         }
-   // snapshotFlow { pagerState.currentPage } creates a Flow of the current page snapshot.
+    }
+    // snapshotFlow { pagerState.currentPage } creates a Flow of the current page snapshot.
     //This means that the flow will only emit a new value whenever the current page changes.
-    
 
-        Scaffold(
-            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-            topBar = {
-                NewsScreenTopBar(
-                    scrollBehavior=scrollBehavior,
-                    onSearchIconClicked = {}
-                )
-            },
-        )
-        { padding ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(padding)
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            NewsScreenTopBar(
+                scrollBehavior = scrollBehavior,
+                onSearchIconClicked = {}
+            )
+        },
+    )
+    { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+
+            CategoryTabRow(
+                pagerState = pagerState,
+                categories = categories,
+                onTabSelected = { index ->
+                    coroutineScope.launch { pagerState.animateScrollToPage(index) }
+                })
+
+            HorizontalPager(
+                pageCount = categories.size,
+                state = pagerState
             ) {
-
-                CategoryTabRow(
-                     pagerState = pagerState,
-                    categories = categories,
-                    onTabSelected = { index ->
-                        coroutineScope.launch { pagerState.animateScrollToPage(index) }
-                    })
-
-                HorizontalPager(
-                    pageCount = categories.size,
-                    state=pagerState
-                ) {
-                    LazyColumn(
-                        verticalArrangement = Arrangement.spacedBy(16.dp),
-                        contentPadding = PaddingValues(16.dp),
-                    ) {
-                        items(state.articles) { article ->
-                            NewsArticleCard(article = article, onCardClicked = {})
-                        }
-                    }
-                }
-          }
+                 NewsArticleList(
+                     state = state,
+                     onCardClicked = {},
+                     onRetry = {
+                         onEvent(NewsScreenEvent.OnCategoryChanged(state.category))
+                     }
+                 )
+            }
         }
+    }
+
 }
+    @Composable
+    fun NewsArticleList(
+        state: NewsScreenState,
+        onCardClicked : (Article) -> Unit,
+        onRetry:()->Unit,
+    ){
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp),
+        ) {
+            items(state.articles) { article ->
+                NewsArticleCard(
+                    article = article,
+                    onCardClicked = onCardClicked
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+                if(state.isLoading)
+                {
+                    CircularProgressIndicator()
+                }
+                if(state.error!=null)
+                {
+                    RetryContent(
+                        error = state.error,
+                        onRetry = onRetry
+                    )
+                }
+        }
+    }
+
 
 
 // state : Any value that can change during the usage of the app
--- app/src/main/java/com/music/newsapp/presentation/news_screen/NewsScreenViewModel.kt
+++ app/src/main/java/com/music/newsapp/presentation/news_screen/NewsScreenViewModel.kt
@@ -43,15 +43,24 @@ class NewsScreenViewModel @Inject constructor(
 
     private fun getNewsArticles(category: String) {
         viewModelScope.launch {
+            state=state.copy(isLoading = true)
             val result = newsRepository.getTopHeadlines(category = category)
             when (result) {
                 is Resource.Success -> {
                     state = state.copy(
-                        articles = result.data ?: emptyList()
+                        articles = result.data ?: emptyList(),
+                        isLoading = false,
+                        error = null
                     )
                 }
 
-                is Resource.Error -> TODO()
+                is Resource.Error -> {
+                    state.copy(
+                        error=result.message,
+                        isLoading = false,
+                        articles = emptyList()
+                    )
+                }
 
             }
         }
